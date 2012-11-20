c08009baa0c33b7e1659a8c12e4c92bff060215d
Thomas Broyer <t.broyer@ltgt.net>
2012-11-20 17:12:15 +0100

    Move Base64Utils to com.google.gwt.util.tools.shared
    This class is used in RequestFactory in addition to GWT-RPC, so we need a
    place for it that both RF and RPC can depend onto once they'll be split
    out in their own JARs.
    
    git fetch https://gwt.googlesource.com/gwt refs/changes/00/1200/1 && git cherry-pick FETCH_HEAD
    
    Change-Id: Id7d11a11bd9d454b8ed8a2b39d3ca302ae0623d6
    
    Conflicts:
    	user/src/com/google/web/bindery/requestfactory/vm/impl/OperationKey.java

--- user/test/com/google/gwt/user/RpcSuiteNoBrowser.java
+++ user/test/com/google/gwt/user/RpcSuiteNoBrowser.java
@@ -22,7 +22,6 @@ import com.google.gwt.user.client.rpc.impl.ClientSerializationStreamReaderTest;
 import com.google.gwt.user.rebind.rpc.BlacklistTypeFilterTest;
 import com.google.gwt.user.rebind.rpc.SerializableTypeOracleBuilderTest;
 import com.google.gwt.user.rebind.rpc.TypeHierarchyUtilsTest;
-import com.google.gwt.user.server.Base64Test;
 import com.google.gwt.user.server.UtilTest;
 import com.google.gwt.user.server.rpc.AbstractXsrfProtectedServiceServletTest;
 import com.google.gwt.user.server.rpc.RPCRequestTest;
